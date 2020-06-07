a23548c5b61c6317f02e6bac31d81ec6190dd8f9
Impeccable Tester <15992558+impeccable-tester@users.noreply.github.com>
2020-06-07 12:13:48 +0800

    Initial commit

---
+++ H2O/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Data.Xml.Dom;
+using Windows.Storage;
+using System.Xml.Linq;
+using Windows.UI.Notifications;
+using Windows.Storage.Streams;
+using System.Threading.Tasks;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace H2O
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            //InitializeComponent();
+            _ = DoWork();
+           
+        }
+
+        private static async Task DoWork()
+        {
+            await Task.Run(async () =>
+            {
+                while (true)
+                {
+                    DoMoreWork();
+                    await Task.Delay(new TimeSpan(1,0,0));
+                    
+                }
+            });
+        }
+
+        private static void DoMoreWork()
+        {
+            ShowToast();
+        }
+
+        private static async void ShowToast()
+        {
+            XmlDocument doc = new XmlDocument();
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Toast.xml", UriKind.Absolute));
+            IRandomAccessStream readStream = await file.OpenAsync(FileAccessMode.Read);
+            XDocument xmldoc = XDocument.Load(readStream.AsStreamForRead());
+            var toastTemplate = xmldoc.ToString();
+            doc.LoadXml(toastTemplate);
+            var toast = new ToastNotification(doc);
+            _ = ToastNotificationManager.CreateToastNotifier();
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ShowToast();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# H2O
+![H2O](https://github.com/impeccable-tester/H2O/blob/master/H2O/Assets/DrinkWater.jpg)
+
+Simple application that sends a toast message to windows users reminding 'em to stretch and drink water at regular intervals!
+
+Extract the zip and run the H2O application.
+
+Hourly reminders can be snoozed 
+
+Installation
+--------------
+On a windows box extract the H2O zip and double click the H2O_1.0.1.0_x86_x64_arm APPXBUNDLE file.
